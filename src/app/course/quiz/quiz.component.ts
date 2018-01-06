2207491d1aeeeb9d6c4aee4c208b558c073654c8
Pascal Perle <Pascal@FamiliePerle.de>
2018-01-06 12:33:33 +0100

    Student can solve the test and send to backend close #13

--- src/app/course/quiz/quiz.component.ts
+++ src/app/course/quiz/quiz.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
-import { ActivatedRoute } from '@angular/router';
-import { Message, Quiz, Status } from '../../shared/RestResults';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Message, Quiz, Status, UserAnswer } from '../../shared/RestResults';
 import { SessionDataManagerService } from '../../shared/session-data-manager.service';
 import { MatSnackBar } from '@angular/material';
 import { HttpLoginService } from '../../shared/services/http-login.service';
@@ -13,13 +13,14 @@ import { HttpLoginService } from '../../shared/services/http-login.service';
 })
 export class QuizComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
-
   quiz: Quiz;
+  userAnswers: UserAnswer[] = [];
 
   constructor(private sessionDataManagerService: SessionDataManagerService,
               private snackBar: MatSnackBar,
               private http: HttpLoginService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -38,6 +39,7 @@ export class QuizComponent implements OnInit, OnDestroy {
         if (message.status === Status.SUCCESS) {
           this.sessionDataManagerService.quiz = message.data as Quiz;
           this.quiz = this.sessionDataManagerService.quiz;
+          this.initUserAnswers();
         } else {
           this.snackBar.open((message.data as Error).message, '', {
             duration: 3500,
@@ -52,7 +54,39 @@ export class QuizComponent implements OnInit, OnDestroy {
     }
   }
 
+  onUpdateCheckedSingeAnswer(questionIndex, answerIndex) {
+    this.userAnswers[questionIndex].givenAnswerIndizies = [answerIndex];
+  }
+
+  onUpdateCheckedMultipleAnswers(questionIndex, answerIndex) {
+    const position = this.userAnswers[questionIndex].givenAnswerIndizies.indexOf(answerIndex);
+    if (position > -1) {
+      this.userAnswers[questionIndex].givenAnswerIndizies.splice(position, 1);
+    } else {
+      this.userAnswers[questionIndex].givenAnswerIndizies.push(answerIndex);
+    }
+  }
+
+  private initUserAnswers() {
+    this.quiz.questions.forEach(question => {
+      this.userAnswers.push({questionId: question._id, givenAnswerIndizies: []});
+    });
+  }
+
   sendResults() {
-    console.log('send results to backend');
+    this.http.post('http://localhost:3000/users/' + this.sessionDataManagerService.user._id + '/quizs',
+      {
+        quizId: this.quiz._id,
+        answers: this.userAnswers
+      }
+    ).subscribe((message: Message) => {
+      if (message.status === Status.SUCCESS) {
+        this.router.navigate(['courses']);
+      } else {
+        this.snackBar.open((message.data as Error).message, '', {
+          duration: 3500,
+        });
+      }
+    });
   }
 }
